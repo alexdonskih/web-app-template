2e7d39f56bdf97dc6024c21a93fbf93b16f1f939
alexd <500minut@gmail.com>
2014-07-22 11:32:20 +0400

    init

---
+++ backend.php
@@ -0,0 +1,10 @@
+<?php
+$data = $_GET;
+switch($data['action']){
+	case 'someaction':
+		// do stuff
+	break;
+	case 'otheraction':
+		// do other stuff
+	break;
+}
---
+++ index.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="ru">
+<head>
+	<meta charset="UTF-8">
+	<title>Document</title>
+	<link rel="stylesheet" href="css/style.css" type="text/css">
+	<link rel="shortcut icon" href="favicon.ico" type="image/icon">
+</head>
+<body>
+<!-- your html here -->
+<script src="//code.jquery.com/jquery-1.11.0.min.js"></script>
+<script src="//code.jquery.com/jquery-migrate-1.2.1.min.js"></script>
+<script src="js/script.js"></script>
+</body>
+</html>
