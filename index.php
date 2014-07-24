73629bf247dd5117098e90b7dab57a8f708c8293
Zotoff <paulzotoff@gmail.com>
2014-07-24 20:40:25 +0400

    Some text added

--- index.php
+++ index.php
@@ -9,6 +9,7 @@
 <body>
 <!-- your html here -->
 <h1>Fucking header</h1>
+<p>Let's add some fucking text</p>
 <script src="//code.jquery.com/jquery-1.11.0.min.js"></script>
 <script src="//code.jquery.com/jquery-migrate-1.2.1.min.js"></script>
 <script src="js/script.js"></script>
